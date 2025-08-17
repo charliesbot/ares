5f832b6e0f7ca705290692c48d42504d5adb6703
Charlie L <charliesbot@gmail.com>
2025-08-17 16:33:07 -0700

    setup spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,34 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.spotless)
+}
+
+spotless {
+  kotlin {
+    target("**/*.kt")
+    targetExclude("**/build/**/*.kt")
+    ktfmt().googleStyle()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
+
+  kotlinGradle {
+    target("*.gradle.kts")
+    ktfmt().googleStyle()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
+
+  java {
+    target("**/*.java")
+    targetExclude("**/build/**/*.java")
+    googleJavaFormat().aosp()
+    removeUnusedImports()
+    trimTrailingWhitespace()
+    endWithNewline()
+  }
 }
